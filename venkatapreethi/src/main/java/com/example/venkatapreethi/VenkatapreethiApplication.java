9164a37bf0bb69aa36ea48ea992d4f6919ba7539
unknown <vvpreethi578@gmail.com>
2019-06-21 15:27:51 +0530

    first commit

---
+++ venkatapreethi/src/main/java/com/example/venkatapreethi/VenkatapreethiApplication.java
@@ -0,0 +1,13 @@
+package com.example.venkatapreethi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenkatapreethiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenkatapreethiApplication.class, args);
+	}
+
+}
---
+++ venkatapreethi/src/test/java/com/example/venkatapreethi/VenkatapreethiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.venkatapreethi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VenkatapreethiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
